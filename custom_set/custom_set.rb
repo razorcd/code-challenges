df78353cc9d0ec5f55c8ce2a95ddea7e692eb0cc
razor <razorcd@yahoo.com>
2016-05-30 17:01:54 +0200

    init CustomSet

---
+++ custom_set/custom_set.rb
@@ -0,0 +1,5 @@
+class CustomSet
+
+  def initialize
+  end
+end
---
+++ tests/custom_set/test_custom_set.rb
@@ -0,0 +1,6 @@
+require 'test/unit'
+require_relative '../../custom_set/custom_set'
+
+class CustomSetTest < Test::Unit::TestCase
+
+end
